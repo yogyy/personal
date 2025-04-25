6a682e440110f799f411bae6c663f055d664375a
yogyy <yogimr261@gmail.com>
2025-04-25 22:59:12 +0700

    feat: add  descriptions for clarity and detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,9 @@ export default function Home() {
             <article>
               <div>
                 <h1 className="mt-14 text-accent">Tech Stack</h1>
+                <p className="mt-2">
+                  Here are the technologies I use regularly to build a projects:
+                </p>
                 <TechSection className="mt-6 text-text/75" />
               </div>
             </article>
@@ -35,7 +38,7 @@ export default function Home() {
           <Reveal ref={ref} inView={inView}>
             <article>
               <h1 className="text-accent">Features Project</h1>
-              <p className="mt-2">Showcase of my works on frontend development.</p>
+              <p className="mt-2">Showcasing my expertise in frontend and fullstack development.</p>
               <ul className="mt-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
                 {allProjects.map(project => (
                   <ProjectCard key={project.slug} project={project} />
