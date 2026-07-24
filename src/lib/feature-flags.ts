50a760469dfd8db5f9cb5e57ae5590426013eb9a
yogyy <yogimr261@gmail.com>
2026-07-25 02:10:20 +0700

    fix: update libs

--- src/lib/feature-flags.ts
+++
@@ -1,8 +0,0 @@
-import { PostHog } from 'posthog-node';
-import { env } from '@/env';
-
-const client = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
-  host: 'https://app.posthog.com',
-});
-
-export { client };
